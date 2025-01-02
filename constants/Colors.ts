8a8fc8c00966d10ab76cbcf31c7fa52b0eacacd3
ytzhou <zhouyuetong@xinqijitech.com>
2025-01-02 11:25:48 +0800

    颜色添加

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -11,6 +11,76 @@ const tintColorDark = '#fff';
 export const BLUE = '#4A90E2';
 
 
+export const PRIMARY_BLUE = '#3C7CFC';
+
+export const BACKGROUND_GRAY = '#F5F7F9';
+
+export const GRAY_DARK = '#222222';
+
+export const GRAY_MEDIUM = '#B8B8B8';
+
+export const GRAY_LIGHTER = '#888888'; 
+
+export const GRAY_LIGHTEST = '#C7C7C7';
+
+export const ORANGE = '#F9512E';
+
+export const GRAY_DARKER = '#313131';
+
+export const BROWN = '#4F1A05';
+
+export const LIGHT_BLUE = '#F7FAFF';
+
+export const ORANGE_LIGHT = '#FF7430';
+
+export const GREEN = '#04B578';
+
+export const RED = '#FC1B24';
+
+export const BEIGE = '#FFDDB3';
+
+export const BROWN_DARK = '#433A30';
+
+export const BROWN_LIGHT = '#8A6F57';
+
+export const BROWN_MEDIUM = '#988164';
+
+export const BROWN_BEIGE = '#B2967D';
+
+export const BROWN_SAND = '#A18E7B';
+
+export const BROWN_LIGHT_BEIGE = '#CFB9A2';
+
+export const BROWN_CREAM = '#E9D2B7';
+
+export const BROWN_LIGHT_CREAM = '#FBE2C6';
+
+export const BROWN_TAUPE = '#8D7B6D';
+
+export const BROWN_DARKEST = '#1E140A';
+
+export const BROWN_SAND_LIGHT = '#FADBB6';
+
+export const BLUE_LINK = '#1677FF';
+
+export const BROWN_SAND_LIGHTER = '#FFE5C1';
+
+export const BROWN_PEACH = '#FFD3AE';
+
+export const BEIGE_LIGHT = '#FFDDB3';
+
+export const BROWN_PEACH_LIGHT = '#F5C9A7';
+
+export const BROWN_SAND_LIGHTEST = '#FFE4C0';
+
+export const BROWN_SAND_LIGHTEST_2 = '#FFE6D0';
+
+export const BROWN_PEACH_MEDIUM = '#FFD3A9';
+
+export const BROWN_PEACH_LIGHT_2 = '#FFD5AD';
+
+export const BROWN_MEDIUM_DARK = '#7A6147';
+
 export const GRAY = '#666666';
 
 export const BLACK = '#000000';
